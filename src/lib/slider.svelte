52f038217baac5e9409af34008a7011d16343356
felippe paulo <felippepaulo0606@gmail.com>
2024-07-05 01:06:54 -0300

    style: ajustments

---
+++ src/lib/slider.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+</script>
+    <input 
+        type="range" 
+        min="0"
+        max="100"
+        >
+    
+<style lang="scss">
+
+</style>
